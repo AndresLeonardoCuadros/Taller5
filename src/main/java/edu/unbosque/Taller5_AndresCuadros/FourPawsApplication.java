22aa2741d3d2c6da24cc475a7e2658dec806f4ba
AndresLeonardoCuadros <71888752+AndresLeonardoCuadros@users.noreply.github.com>
2021-11-12 15:01:08 -0500

    Add files via upload
    Creación base de los archivos del proyecto

---
+++ src/main/java/edu/unbosque/Taller5_AndresCuadros/FourPawsApplication.java
@@ -0,0 +1,8 @@
+package edu.unbosque.Taller5_AndresCuadros;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class FourPawsApplication extends Application{
+}
